0b3c748196ed85d98e87b4734efd176baa2a4435
simondlevy <simon.d.levy@gmail.com>
2023-05-29 16:32:52 -0400

    Cleanup

--- linux/client.c
+++ linux/client.c
@@ -10,7 +10,15 @@
 #define CLIENT_SOCKET "client.sock"
 #define BIND_SOCKET "bind.sock"
 
-int main(int argc, char* argv[]) {
+static void error(const char * msg)
+{
+    perror(msg);
+    unlink(CLIENT_SOCKET);
+    exit(1);
+}
+
+int main(int argc, char* argv[]) 
+{
     char Buf[100] = { 0 };
     ssize_t BytesRecvd = 0;
     struct sockaddr_un clientAddr = { 0 };
@@ -22,21 +30,20 @@ int main(int argc, char* argv[]) {
     socklen_t addrLen = 0;
 
     if ((clientFd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("clientFd socket error");
-        goto ErrorExit;
+        error("clientFd socket error");
     }
 
     clientAddr.sun_family = AF_UNIX;
     strncpy(clientAddr.sun_path, CLIENT_SOCKET, sizeof(clientAddr.sun_path) - 1);
     if (bind(clientFd, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) == -1) {
-        perror("server bind error");
-        goto ErrorExit;
+        error("server bind error");
+        unlink(CLIENT_SOCKET);
+        return 0;
     }
 
     addrLen = sizeof(clientAddr);
     if (getsockname(clientFd, (struct sockaddr*)&clientAddr, &addrLen) == -1) {
-        perror("getsockname(client)");
-        goto ErrorExit;
+        error("getsockname(client)");
     }
 
     printf("getsockname returned: %s, addressize: %d\n", clientAddr.sun_path, addrLen);
@@ -44,26 +51,20 @@ int main(int argc, char* argv[]) {
     strncpy(serverAddr.sun_path, SERVER_SOCKET, sizeof(serverAddr.sun_path));
     printf("client: connecting to %s\n", SERVER_SOCKET);
     if (connect(clientFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
-        perror("connect error");
-        goto ErrorExit;
+        error("connect error");
     }
 
     printf("client: connected to the server\n");
     addrLen = sizeof(clientAddr);
     if (getpeername(clientFd, (struct sockaddr*)&clientAddr, &addrLen) == -1) {
-        perror("getpeername(client)");
-        goto ErrorExit;
+        error("getpeername(client)");
     }
 
     printf("getpeername returned: %s, addressize: %d\n", clientAddr.sun_path, addrLen);
     if ((BytesRecvd = recv(clientFd, Buf, sizeof(Buf), 0)) == -1) {
-        perror("recv");
-        goto ErrorExit;
+        error("recv");
     }
 
     printf("received: %zd bytes, %s\n", BytesRecvd, Buf);
-
-ErrorExit:
-    unlink(CLIENT_SOCKET);
     return 0;
 }
